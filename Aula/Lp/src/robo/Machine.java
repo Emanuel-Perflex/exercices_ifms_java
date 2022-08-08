384ccddd0a849916a4d5b4ecf62dbc0b6b6f30d5
Emanuel Barbosa <emanuelbarrbosati@gmail.com>
2022-08-08 00:24:34 -0400

    Commit 5.0

--- Aula/Lp/src/robo/Machine.java
+++ Aula/Lp/src/robo/Machine.java
@@ -1,13 +1,31 @@
 package robo;
 
+import java.util.Scanner;
+
 public class Machine {
     
 
     public static void main(String[] args) {
         Robo maquina = new Robo();
         
+        Scanner opcao = new Scanner(System.in);
+        
+
         while (true){
-            maquina.leitura();
+
+            System.out.println("\nGostaria de controlar por joystick?");
+            String leitura = opcao.nextLine(); 
+
+            if (leitura.equals("Sim") || leitura.equals("sim")){
+                while (true) {
+                    maquina.leitura();
+                    maquina.distancia();    
+                }
+            } else {
+                System.out.println("Insira as coordenadas iniciais e a do destino");
+                maquina.coordenadas();
+                break;
+            }
         }
     }
 }
--- Aula/Lp/src/robo/Robo.java
+++ Aula/Lp/src/robo/Robo.java
@@ -1,12 +1,14 @@
 package robo;
 
 import java.util.Scanner;
+import javax.imageio.plugins.tiff.GeoTIFFTagSet;
 
 public class Robo {
     //lados
     private int x = 0;
     private int y = 0;
-
+    private int calcX = 0;
+    private int calcY = 0;
 
     public int getX() {
         return x;
@@ -14,6 +16,14 @@ public class Robo {
 
     public int getY() {
         return y;
+    }
+
+    public int getCalcX() {
+        return calcX;
+    }
+
+    public int getCalcY() {
+        return calcY;
     }
 
     public void setX(int x) {
@@ -24,22 +34,34 @@ public class Robo {
         this.y = y;
     }
 
+    public void setCalcX(int calcX) {
+        this.calcX = calcX;
+    }
+
+    public void setCalcY(int calcY) {
+        this.calcY = calcY;
+    }
+
     public void cima (){
+        this.calcY = this.y;
         this.y++;
         System.out.println("\nA posição agora é: (" + x + "," + y + ")");
     }
 
     public void baixo (){
+        this.calcY = this.y;
         this.y--;
         System.out.println("\nA posição agora é: (" + x + "," + y + ")");
     }
     
     public void esquerda (){
+        this.calcX = this.x;
         this.x--;
         System.out.println("\nA posição agora é: (" + x + "," + y + ")");
     }
 
     public void direita (){
+        this.calcX = this.x;
         this.x++;
         System.out.println("\nA posição agora é: (" + x + "," + y + ")");
     }
@@ -57,5 +79,31 @@ public class Robo {
         } else if (comando.equals("d")){
             direita();;
         }
+    }
+
+    public void distancia (){
+        double distancia = Math.sqrt(Math.pow(x - calcX, 2) + Math.pow(y - calcY, 2));
+        System.out.println("A distância percorrida foi de: (" + distancia + ")");
+        distancia = 0;
+        this.calcX = 0;
+        this.calcY = 0;
+    }
+
+    public void coordenadas (){
+        Scanner ler = new Scanner(System.in);
+        int x1 = ler.nextInt();
+        int x2 = ler.nextInt();
+        int y1 = ler.nextInt();
+        int y2 = ler.nextInt();
+        
+        this.x = x1;
+        this.calcX = x2;
+        this.y = y1;
+        this.calcY = y2;
+
+        double distancia = Math.sqrt(Math.pow(x - calcX, 2) + Math.pow(y - calcY, 2));
+        System.out.println("As cordenadas iniciais inseridas foram: (" + x + "," + calcX + ")");
+        System.out.println("E as coordenadas finais inseridas foram: (" + y + "," + calcY + ")");
+        System.out.println("A distância percorrida foi de: (" + distancia + ")");
     }
 }
