4b51fbdd0264ab36be92f8cd5d496f56a70230a6
Emanuel <emanuelbarbosati@gmail.com>
2022-08-16 21:12:30 -0400

    anotações Collections 1.0

--- Aula/Poo/collections/MinhaColecao.java
+++ Aula/Poo/collections/MinhaColecao.java
@@ -3,8 +3,19 @@ package Aula.poo.collections;
 public class MinhaColecao {
     public static void main(String[] args) {
 
-        Livro livros[];
+        //collections = array de objetos;
+        //nas collections dá pra fazer o vetor de gato e sapato, o que com um array normal não é possível
+ 
+        int vetor[] = new int[4]; //Dá muito trabalho, tem que estabelecer for para copiar posições e dá muito trabalho;
 
-        Livro li = new Livro("Minha coleção")
+        //estrutura 
+        //Uma list é uma classe que herda de coleção
+        //Interface é classe abstrata 
+        
+        //Estudar list por conta
+        
+        //Estrutura de list
+        //Declaração de collection
+        List<livro> livros = new ArrayList<livro>();
     }
 }
