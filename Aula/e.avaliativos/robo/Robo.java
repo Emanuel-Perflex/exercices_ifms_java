8adcdd5e06a65089111617dc792125f624e9f16f
Emanuel-Perflex <emanuelbarbosati@gmail.com>
2022-08-24 18:57:08 -0400

    Otimizações robo.java

--- Aula/e.avaliativos/robo/Robo.java
+++ Aula/e.avaliativos/robo/Robo.java
@@ -73,9 +73,7 @@ public class Robo {
 
 //--------------------Métodos adicionais------------------------------//
     /* 
-
     //Método descontinuado por hora, ao invés disso um while true vai ler as setas e retornar a posição;
-
     public void leitura (){
         Scanner ler = new Scanner(System.in);
         String comando = ler.nextLine();
@@ -105,10 +103,19 @@ public class Robo {
     //Função para caso o usuário queira sair de um ponto especifico e ir para outro;
     public void coordenadas (){
         Scanner ler = new Scanner(System.in);
-        int x1 = this.x;
-        int x2 = this.calcX;
-        int y1 = this.y;
-        int y2 = this.calcY;
+        //Necessário zerar para toda utilização;
+        this.calcX = 0;
+        this.calcY = 0;
+        this.x = 0;
+        this.y = 0;
+        //Procedimentos//
+        
+        //Xo e Yo;
+        this.calcX = ler.nextInt();
+        this.calcY = ler.nextInt();
+        //X e Y
+        this.x = ler.nextInt();
+        this.y = ler.nextInt();
 
         double distancia = Math.sqrt(Math.pow(x - calcX, 2) + Math.pow(y - calcY, 2));
         System.out.println("As cordenadas iniciais inseridas foram: (" + x + "," + calcX + ")");
