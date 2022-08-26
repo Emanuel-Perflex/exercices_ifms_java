8d6e2bb27de138fc8fde66e943c80e766ce67999
Emanuel-Perflex <emanuelbarbosati@gmail.com>
2022-08-26 02:50:18 -0400

    mini changes

--- Aula/e.avaliativos/dashboard/SistemaCadastro.java
+++ Aula/e.avaliativos/dashboard/SistemaCadastro.java
@@ -1,9 +1,39 @@
 package dashboard;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
+import javax.swing.JFrame;
+
+//Decidi fazer tudo com Swing, Deus me ajude, Amém;
 public class SistemaCadastro {
-  
+    JFrame main = new JFrame();
+
+    SistemaCadastro(){
+        main.setTitle("Cadastro Bibliotecário");
+        main.setSize(800, 600);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.pack();
+        main.setLocationRelativeTo(null); 
+        main.setVisible(true);
+    }
+       
+        
+
+    public static void main(String[] args) {
+        new SistemaCadastro();
+    }
 }
+
+
+/*
+
+    "|                 Escolha um opção:                  |",
+    "| -------------------------------------------------- | ",
+    "| 1 - Cadastrar Livro                                |",
+    "| 2 - Cadastrar Funcionario                          |",
+    "| 3 - Cadastrar Estudante                            |",
+    "| 4 - Cadastrar Autor                                |",
+    "| 5 - Realizar Emprestimo                            |",
+    "| 6 - Receber Emprestimo                             |",
+    "| 0 - Sair                                           |",
+    "| -------------------------------------------------- |", 
+ 
+ */
