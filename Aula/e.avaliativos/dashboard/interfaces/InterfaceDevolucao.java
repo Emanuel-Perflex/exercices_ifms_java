307904491faf901b2bbfaa24548e18d1af8f51c9
Emanuel-Perflex <emanuelbarbosati@gmail.com>
2022-08-27 00:13:59 -0400

    Mini changes

--- Aula/e.avaliativos/dashboard/interfaces/InterfaceDevolucao.java
+++ Aula/e.avaliativos/dashboard/interfaces/InterfaceDevolucao.java
@@ -1,5 +1,30 @@
 package dashboard.interfaces;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.GridLayout;
+
 public class InterfaceDevolucao {
-    
+    //Jframe main
+    JFrame main = new JFrame();
+
+    public InterfaceDevolucao (){
+        painelAutor.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        painelAutor.setLayout(new GridLayout(0, 1));
+
+        //--------------------------------Configuração Da janela--------------------------------//
+        main.add(painelAutor, BorderLayout.CENTER);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.pack();
+        main.setLocationRelativeTo(null); 
+        main.setTitle("Cadastro Bibliotecário");
+        main.setSize(500, 300);
+        main.setVisible(true);        
+    }
+
 }
--- Aula/e.avaliativos/dashboard/interfaces/InterfaceEstudante.java
+++ Aula/e.avaliativos/dashboard/interfaces/InterfaceEstudante.java
@@ -1,5 +1,32 @@
 package dashboard.interfaces;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.GridLayout;
+
 public class InterfaceEstudante {
+    //JFrame
+    JFrame main = new JFrame();
+    //Painel main
+    JPanel painelEstudante = new JPanel();
     
+
+    public InterfaceEstudante (){
+        painelEstudante.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        painelEstudante.setLayout(new GridLayout(0, 1));
+
+        //--------------------------------Configuração Da janela--------------------------------//
+        main.add(painelEstudante, BorderLayout.CENTER);
+        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        main.pack();
+        main.setLocationRelativeTo(null); 
+        main.setTitle("Cadastro Bibliotecário");
+        main.setSize(500, 300);
+        main.setVisible(true); 
+    }
 }
