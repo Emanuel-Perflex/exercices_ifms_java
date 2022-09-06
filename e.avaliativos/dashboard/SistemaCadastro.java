c236ed5fa9ab5d808885e37754813799d4049a00
Emanuel <93679887+Emanuel-Perflex@users.noreply.github.com>
2022-09-05 20:46:55 -0400

    Update SistemaCadastro.java

--- e.avaliativos/dashboard/SistemaCadastro.java
+++ e.avaliativos/dashboard/SistemaCadastro.java
@@ -2,7 +2,6 @@ package dashboard;
 
 import dashboard.interfaces.InterfaceGeral;
 
-//Decidi fazer tudo com Swing, Deus me ajude, Amém;
 public class SistemaCadastro {        
 
     public static void main(String[] args) {
